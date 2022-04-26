cda566709b15b3734578b5b1ace005cfb360d5b2
MustafaMerchant21 <mr.merchant53@gmail.com>
2022-04-26 17:47:53 +0530

    Code analyze and content addition

---
+++ script.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import Slide from 'react-reveal/Slide';
+class SlideExample extends React.Component {
+    render() {
+      return (
+        <div>
+          <Slide left>
+            <div></div>
+          </Slide>
+        </div>
+      );
+    }
+  }
